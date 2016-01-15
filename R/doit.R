91c263ad6d631b0c14d2fb3ddaf311cfd6195445
Scott Chamberlain <myrmecocystus@gmail.com>
2016-01-15 08:43:59 -0800

    tidy internal fxns doit and parseit

--- R/doit.R
+++ R/doit.R
@@ -1,18 +1,18 @@
 # doit() turn structured description into which bands to operate on how
 doit <- function(obj, args) {
   #first do selection/subsetting
-  seltext = ""
+  seltext <- ""
   if (!is.na(args$startdate)) {
-    seltext = args$startdate
+    seltext <- args$startdate
   } else {
-    seltext = ""
+    seltext <- ""
   }
   if (!is.na(args$enddate)) {
     if (seltext != "") {
       seltext <- paste(seltext, "/", args$enddate, sep = "")
     }
     else {
-      seltext = args$enddate
+      seltext <- args$enddate
     }
   }
   outobj = obj
@@ -21,9 +21,9 @@ doit <- function(obj, args) {
     if (length(newts) == 0) {
       stop("TimeRaster subscripted to zero layers", call. = FALSE)
     }
-    outobj = outobj[[as.numeric(newts)]]   ####
-    newts[] = 1:length(newts)
-    outobj@ts = newts
+    outobj <- outobj[[as.numeric(newts)]]
+    newts[] <- 1:length(newts)
+    outobj@ts <- newts
     names(outobj) <- index(outobj@ts)
   }
 
@@ -31,18 +31,18 @@ doit <- function(obj, args) {
   #args$  agglevel  aggfunc aggkeepna
   if (!is.na(args$agglevel)) {
     if (args$aggup) {
-      ep = endpoints(outobj@ts,tolower(args$agglevel))
-      idx = floor(approx(ep + 1, 1:length(ep), 1:length(outobj@ts))$y)
-      oldts = outobj@ts
-      outobj = stack(stackApply(outobj, idx, tolower(args$aggfunc), na.rm = !args$aggkeepna))   ####
-      newts = oldts[ep[-length(ep)] + 1]
-      newts[] = 1:length(newts)
-      outobj = new("TimeRaster", outobj, ts = newts)
+      ep <- endpoints(outobj@ts,tolower(args$agglevel))
+      idx <- floor(approx(ep + 1, 1:length(ep), 1:length(outobj@ts))$y)
+      oldts <- outobj@ts
+      outobj <- stack(stackApply(outobj, idx, tolower(args$aggfunc), na.rm = !args$aggkeepna))   ####
+      newts <- oldts[ep[-length(ep)] + 1]
+      newts[] <- 1:length(newts)
+      outobj <- new("TimeRaster", outobj, ts = newts)
       #DOITHERE - set raster layer names
       names(outobj) <- index(outobj@ts)
-    }
-    else
+    } else {
       stop("Error DOWNTO not implemented yet", call. = FALSE)
+    }
   }
 
   #OK now do periodicity
@@ -53,33 +53,32 @@ doit <- function(obj, args) {
       if (curperiod != 1) {
         stop("Invalid across month", call. = FALSE)
       }
-      idx = .indexmday(outobj@ts)
-    }
-    else if (args$acrosslevel == "YEAR") {
+      idx <- .indexmday(outobj@ts)
+    } else if (args$acrosslevel == "YEAR") {
       if (curperiod == 31) { #monthly data
-        idx = .indexmon(outobj@ts) + 1
+        idx <- .indexmon(outobj@ts) + 1
       } else if (curperiod == 7) {#weeklydata
-        idx = .indexweek(outobj@ts)
-        idx = idx - min(idx) + 1 #week is relative to epoch
+        idx <- .indexweek(outobj@ts)
+        idx <- idx - min(idx) + 1 #week is relative to epoch
       }
       else if (curperiod == 1) { #daily
-        idx = .indexyday(outobj@ts) + 1
+        idx <- .indexyday(outobj@ts) + 1
       } else {
         stop("invalid across year", call. = FALSE)
       }
-    }
-    else
-      stop(paste("Inavlid across level:",args$acrosslevel), call. = FALSE)
-    #browser() #some errors in index/newdates
-    oldts = outobj@ts
-    outobj = stack(stackApply(outobj, idx, tolower(args$acrossfunc), na.rm = !args$acrosskeepna))  ####
-    if (curperiod == 31) {
-      newdates = seq(as.Date("2000-1-1") + 0.4, by = 'month', length.out = 12)
     } else {
-      newdates = as.Date("2000-1-1") + 0.4 + unique(idx - 1) * curperiod
+      stop(paste("Inavlid across level:",args$acrosslevel), call. = FALSE)
     }
-    newts = xts(1:length(newdates),newdates)
-    outobj = new("TimeRaster",outobj, ts = newts)
+    #browser() #some errors in index/newdates
+    oldts <- outobj@ts
+    outobj <- stack(stackApply(outobj, idx, tolower(args$acrossfunc), na.rm = !args$acrosskeepna))  ####
+    if (curperiod == 31) {
+      newdates <- seq(as.Date("2000-1-1") + 0.4, by = 'month', length.out = 12)
+    } else {
+      newdates <- as.Date("2000-1-1") + 0.4 + unique(idx - 1) * curperiod
+    }
+    newts <- xts::xts(1:length(newdates), newdates)
+    outobj <- new("TimeRaster",outobj, ts = newts)
     #DOITHERE - set raster layer names
     names(outobj) <- index(outobj@ts)
   } #end if !is.na(args$acrosslevel) - i.e. do ACROSS functionality
--- R/parseit.R
+++ R/parseit.R
@@ -1,92 +1,83 @@
 # parseit()  turns a text string into a structured description of what to do
 parseit <- function(textsub) {
-
   #show is on - need to parse this
-  textsub=gsub("^ *|(?<= ) | *$", "", textsub, perl=T) #remove double spaces
-  #cat("Will parse '",textsub,"'\n")
-  toks=strsplit(textsub," ")[[1]]
+  textsub <- gsub("^ *|(?<= ) | *$", "", textsub, perl = TRUE) #remove double spaces
+  toks <- strsplit(textsub," ")[[1]]
   startdate<-enddate<-aggup<-agglevel<-aggfunc<-aggkeepna<-acrosslevel<-acrossfunc<-acrosskeepna<-NA
-  while (length(toks)>0) {
-    thetok=toupper(toks[[1]])
-    #cat("thetok=",thetok,"\n")
+  while (length(toks) > 0) {
+    thetok <- toupper(toks[[1]])
     #DOIT - allow no start date or enddate
     if (grepl("^[0-9]{4}",thetok)) {
       #DOITHERE - ALLOW no start or end date with TO
       if (is.na(startdate)) {
-        #cat("doing startdate\n")
-        startdate=thetok
-        if (length(toks)>2 && toupper(toks[[2]])=="TO" ) {
-          if (!grepl("^[0-9]{4}",toks[[3]]))
-            stop(cat("Syntax error: invalid 2nd date",toks[[3]]))
-          enddate=toks[[3]]
+        startdate <- thetok
+        if (length(toks) > 2 && toupper(toks[[2]]) == "TO" ) {
+          if (!grepl("^[0-9]{4}", toks[[3]])) {
+            stop("Syntax error: invalid 2nd date ", toks[[3]], call. = FALSE)
+          }
+          enddate <- toks[[3]]
           print(toks)
-          toks=toks[-1]
-          toks=toks[-1] #eat start date & TO token
+          toks <- toks[-1]
+          toks <- toks[-1] #eat start date & TO token
         } #end process enddate
-      } #end working on date
-      else {
-        stop("Syntax error: more than one date")
+      } else {#end working on date
+        stop("Syntax error: more than one date", call. = FALSE)
       }
-    }#end dates
-    else if (grepl("UPTO|DOWNTO",thetok)) {
-      #cat("length=",length(toks),"tok[2]=",toks[[2]],"\n")
-      if (length(toks)>1 && grepl("WEEKS|MONTHS|QUARTERS|YEARS",toupper(toks[[2]]))) {
-        if  (thetok=="UPTO")
-          aggup=TRUE
-        else
-          aggup=FALSE
-        agglevel=toks[[2]]
-        aggfunc="SUM"
-        aggkeepna=FALSE
-        if (length(toks)>3 && toks[[3]]=="BY") {
-          if (grepl("SUM|MEAN|MIN|MAX|VAR|STD|COUNT|MEDIAN",toupper(toks[[4]]))) {
-            aggfunc=toks[[4]]
-            toks=toks[-1] #eat two more to account for BY SUM
-            toks=toks[-1]
-            cat("About check KEEPNA: t1=",toks[[1]],"t2=",toks[[2]],"t3=",toks[[3]],"\n")
-            if (length(toks)>2 && toks[[3]]=="KEEPNA") {
+    } else if (grepl("UPTO|DOWNTO", thetok)) {#end dates
+      if (length(toks) > 1 && grepl("WEEKS|MONTHS|QUARTERS|YEARS", toupper(toks[[2]]))) {
+        aggup <- thetok == "UPTO"
+        agglevel <- toks[[2]]
+        aggfunc <- "SUM"
+        aggkeepna <- FALSE
+        if (length(toks) > 3 && toks[[3]] == "BY") {
+          if (grepl("SUM|MEAN|MIN|MAX|VAR|STD|COUNT|MEDIAN", toupper(toks[[4]]))) {
+            aggfunc <- toks[[4]]
+            toks <- toks[-1] #eat two more to account for BY SUM
+            toks <- toks[-1]
+            cat("About check KEEPNA: t1=", toks[[1]], "t2=", toks[[2]], "t3=", toks[[3]], "\n")
+            if (length(toks) > 2 && toks[[3]] == "KEEPNA") {
               cat("Doing KEEPNA TRUE\n")
-              aggkeepna=TRUE
-              toks=toks[-1]
+              aggkeepna <- TRUE
+              toks <- toks[-1]
             }
+          } else {
+            stop("Syntax error near aggregation BY", call. = FALSE)
           }
-          else
-            stop("Syntax error near aggregation BY")
         } #end if BY
-        toks=toks[-1]
-      }#end if aggto
-      else {
-        stop(cat("Syntax error on aggregation - valid periods are WEEKS, MONTHS, QUARTERS, YEARS"))
+        toks <- toks[-1]
+      } else {
+        stop("Syntax error on aggregation - valid periods are WEEKS, MONTHS, QUARTERS, YEARS", call. = FALSE)
       }
-    }
-    else if (grepl("ACROSS",thetok)) {
-      if (length(toks)>1 && grepl("YEAR|MONTH",toks[[2]])) {
-        acrosslevel=toks[[2]]
-        acrossfunc="SUM"
-        acrosskeepna=FALSE
-        if (length(toks)>3 && toks[[3]]=="BY") {
-          if (grepl("SUM|MEAN|MIN|MAX|VAR|STD|COUNT|MEDIAN",toupper(toks[[4]]))) {
-            acrossfunc=toks[[4]]
-            toks=toks[-1] #eat two more to account for BY SUM
-            toks=toks[-1]
-            if (length(toks)>2 && toks[[3]]=="KEEPNA") {
-              acrosskeepna=TRUE
-              toks=toks[-1]
+    } else if (grepl("ACROSS",thetok)) {
+      if (length(toks) > 1 && grepl("YEAR|MONTH", toks[[2]])) {
+        acrosslevel <- toks[[2]]
+        acrossfunc <- "SUM"
+        acrosskeepna <- FALSE
+        if (length(toks) > 3 && toks[[3]] == "BY") {
+          if (grepl("SUM|MEAN|MIN|MAX|VAR|STD|COUNT|MEDIAN", toupper(toks[[4]]))) {
+            acrossfunc <- toks[[4]]
+            toks <- toks[-1] #eat two more to account for BY SUM
+            toks <- toks[-1]
+            if (length(toks) > 2 && toks[[3]] == "KEEPNA") {
+              acrosskeepna <- TRUE
+              toks <- toks[-1]
             }
+          } else {
+            stop("Syntax error near across BY", call. = FALSE)
           }
-          else
-            stop("Syntax error near across BY")
         } #end if BY
-        toks=toks[-1]
+        toks <- toks[-1]
+      } else {
+        stop("Syntax error near ACROSS", call. = FALSE)
       }
-      else
-        stop("Syntax error near ACROSS")
+    } else {
+      stop("Syntax error: near ", thetok, call. = FALSE)
     }
-    else {
-      stop(cat("Syntax error: near",thetok))
-    }
-    toks=toks[-1] #eat the token
-  } #end while
-  args=list(startdate=startdate,enddate=enddate,aggup=aggup,agglevel=agglevel,aggfunc=aggfunc,aggkeepna=aggkeepna,acrosslevel=acrosslevel,acrossfunc=acrossfunc,acrosskeepna=acrosskeepna)
+    toks <- toks[-1] #eat the token
+  }#end while
+  args <- list(startdate = startdate, enddate = enddate, aggup = aggup,
+               agglevel = agglevel, aggfunc = aggfunc, aggkeepna = aggkeepna,
+               acrosslevel = acrosslevel, acrossfunc = acrossfunc,
+               acrosskeepna = acrosskeepna)
   return(args)
 }
